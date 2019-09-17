97d2cb2afa73b2e9235fa5ad8d22bf98fabb87f4
gary.hung <gary.hung@nexiosoft.com>
2019-09-17 15:30:28 +0800

    workaround for nullable object

--- scope_functions.js
+++ scope_functions.js
@@ -1,19 +1,85 @@
 // Scope Functions for JavaScript
+
 // The Kotlin standard library contains several functions whose sole purpose is to execute a block of code within the context of an object. When you call such a function on an object with a lambda expression provided, it forms a temporary scope. In this scope, you can access the object without its name. Such functions are called scope functions. There are five of them: let, run, with, apply, and also.
 
 // Basically, these functions do the same: execute a block of code on an object. What's different is how this object becomes available inside the block and what is the result of the whole expression.
-// ref: https://kotlinlang.org/docs/reference/scope-functions.html
-// Here's a typical usage of a scope function for JavaScript:
-// these functions only support non-null object
+// more details for official tutorial, ref: https://kotlinlang.org/docs/reference/scope-functions.html
+
+// Here're some typical usages of a scope function for JavaScript:
+
+
+'use strict';
+// the 2 extensions only support non-null object
+
+// return what you return in callback
 Object.prototype.let = function (callback) {
-    return callback.apply(this, [this])
+    return callback.apply(this, [toBooleanValue(this)])
 }
 
+
+// apply is preserved from JS
+// return this
 Object.prototype.applyy = function (callback) {
-    callback.apply(this, [this])
+    callback.apply(this, [toBooleanValue(this)])
     return this
 }
 
+// a transformer for getting the primitive value of a Boolean object
+function toBooleanValue(obj) {
+    return obj instanceof Boolean ? obj.valueOf() : obj
+}
+
+
+// ******************************** example below *******************************************************
+
+// *************************************** run in browser***********************************************
+if (typeof window !== 'undefined') {
+    // before
+    var body = document.querySelector('body');
+    body.innerHTML = "popup_splash_prevent_close_tab".toLowerCase()
+    body.setAttribute('data-tip', "popup_splash_prevent_close_tab_toolip")
+
+    // after
+    document.querySelector('body').applyy(_ => {
+        _.innerHTML = "popup_splash_prevent_close_tab".toLowerCase()
+        _.setAttribute('data-tip', "popup_splash_prevent_close_tab_toolip")
+    });
+}
+
+
+// **************************************can run by Node.js********************************************
+
+// do on a nullable object
+// no works
+try {
+    [3, 2, 3, 44, 23, 423, 4].find(e => e == 1000).let(_ => {
+        // throw a exception cause of "[3, 2, 3, 44, 23, 423, 4].find(e => e == 1000)" is undefined
+    })
+} catch (e) {
+    console.error(e)
+}
+
+// works
+([3, 2, 3, 44, 23, 423, 4].find(e => e == 1000) || false).let(it => {
+    if (it) {
+        // do ...
+    } else {
+        // reach here
+    }
+});
+
+// or
+([3, 2, 3, 44, 23, 423, 4].find(e => e == 44) || false).let(it => {
+    if (it) {
+        // reach here
+    } else {
+        // else do ...
+    }
+})
+
+
+// demo reduce declaration
+// ******************************************************************************************
 class TrashCan {
     recycle() {}
     pick() {}
@@ -25,17 +91,15 @@ class Player {
         console.log('stop')
     }
 
-    play() {
-        console.log('play')
+    play(position) {
+        console.log('play at ', position)
     }
 }
 
-// demo reduce declaration
-// ************************************************************************************
 
 // before
 var player = new Player;
-player.play()
+player.play(5)
 player.stop()
 var trashCan = new TrashCan;
 trashCan.recycle(player);
@@ -46,7 +110,7 @@ trashCan.clear()
 // after
 // var free, isolated scope
 new Player().let(player => {
-    player.play()
+    player.play(888)
     player.stop()
     new TrashCan().let(it => {
         it.recycle(player)
@@ -59,7 +123,7 @@ new Player().let(player => {
 // or
 
 new Player().let(player => {
-    player.play()
+    player.play(777)
     player.stop()
     let trashCan = new TrashCan().applyy(_ => {
         _.recycle(player)
@@ -69,7 +133,72 @@ new Player().let(player => {
     trashCan.clear()
 });
 
+
+// demo return this
+// ***************************************************
+// before
+var player = new Player;
+player.play(6)
+player.stop()
+var trashCan = new TrashCan;
+trashCan.recycle(player);
+trashCan.pick(player);
+player.stop()
+trashCan.clear()
+
+// after
+// return _ in applyy()
+// _ or player or whatever you want to naming
+
+// player is the instance of Player
+var player = new Player().applyy(_ => {
+    _.play(8)
+    _.stop()
+    // no need call return
+})
+
+var trashCan = new TrashCan().applyy(_ => {
+    _.recycle(player);
+    _.pick(player);
+});
+player.stop()
+trashCan.clear()
+
+
+
+// demo return in let
+// ***************************************************
+
+// before
+var positionInSec = ([3, 2, 3, 44, 23, 423, 4].find(e => e == 44) || 0) * 88
+var player = new Player()
+player.play(positionInSec);
+player.stop();
+
+// after
+// var free, chain
+([3, 2, 3, 44, 23, 423, 4].find(e => e == 44) || 0).let(it => it * 88)
+    .let(it =>
+        // it = 44 * 88
+        new Player().applyy(_ => {
+            _.play(it)
+            _.stop()
+        })
+    );
+
+// or
+// return mix let and applyy
+var player = ([3, 2, 3, 44, 23, 423, 4].find(e => e == 44) || 0)
+    .let(it => it * 88)
+    .let(it => new Player().applyy(_ => {
+        _.play(it)
+    }));
+
+player.stop();
+
+// but no need to do this, we will do them all in the scope function
 // demo chain
+
 // *************************************************************************
 // after
 [{
@@ -78,7 +207,7 @@ new Player().let(player => {
 }, {
     id: 'foo2',
     value: 20
-}, ].let(it => {
+}].let(it => {
 
         console.log(it[1].value * 2)
         return it.map((v, i) => i * 10)
@@ -105,15 +234,15 @@ new Player().let(player => {
     })
     .applyy(_ => {
 
-        // window in arrow fn
+        // this is window in arrow fn
         console.log(this)
 
         // console.log('play')
-        _.play()
+        _.play(999)
     })
 
 // before
-
+// ************************************************
 var a = [{
         id: 'foo1',
         value: 10
@@ -121,7 +250,7 @@ var a = [{
     {
         id: 'foo2',
         value: 20
-    },
+    }
 ]
 
 console.log(a[1].value * 2)
@@ -133,4 +262,4 @@ console.log(c)
 var d = c.toFixed()
 var e = new Player
 // console.log('play')
-e.play()
+e.play(d)
